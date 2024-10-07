74b3393451fb1b788e74ef362df471a361474a88
ivasecko48 <robertivasecko2001@gmail.com>
2024-10-07 15:22:19 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,44 @@
+document.addEventListener('DOMContentLoaded', () => {
+	const faqContainer = document.querySelector('.faq-content');
+
+	faqContainer.addEventListener('click', (e) => {
+		const groupHeader = e.target.closest('.faq-group-header');
+
+		if(!groupHeader) return;
+
+		const group = groupHeader.parentElement;
+		const groupBody = group.querySelector('.faq-group-body');
+		const icon = groupHeader.querySelector('i');
+
+		// Toggle icon
+		icon.classList.toggle('fa-plus');
+		icon.classList.toggle('fa-minus');
+
+		// Toggle body visibility
+		groupBody.classList.toggle('open');
+
+		//close other faq bodies
+		const otherGroups = faqContainer.querySelectorAll('.faq-group');
+
+		otherGroups.forEach((otherGroup) => {
+			if(otherGroup !== group) {
+				const otherGroupBody = otherGroup.querySelector('.faq-group-body');
+				const otherIcon = otherGroup.querySelector('.faq-group-header i');
+
+				otherGroupBody.classList.remove('open');
+				otherIcon.classList.remove('fa-minus');
+				otherIcon.classList.add('fa-minus');
+			}
+		});
+	});
+});
+
+//mobile menu
+document.addEventListener('DOMContentLoaded', () => {
+	const mobileMenu = document.querySelector('.mobile-menu');
+	const hamburgerButton = document.querySelector('.hamburger-button');
+
+	hamburgerButton.addEventListener('click', () => mobileMenu.classList.toggle('active')
+
+	);
+});
